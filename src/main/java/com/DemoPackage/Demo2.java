c874c9b173317c14c0aac8823998def290cee22d
Ganesh <Ganesh@LAPTOP-2A1L3UFV>
2025-11-08 08:27:42 +0530

    Second Commit

---
+++ src/main/java/com/DemoPackage/Demo2.java
@@ -0,0 +1,11 @@
+package com.DemoPackage;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Good Morningggggggg!");
+		System.out.println("Have a Nice Day!");
+
+	}
+
+}
